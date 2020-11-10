0e38324b07b48180b492465f90c6888b4c893028
SunBoqiang <51821217+SunBoqiang@users.noreply.github.com>
2020-11-11 02:57:47 +0900

    KSP Transfer degree
    简单计算KSP转移相差

---
+++ TransferWindow.m
@@ -0,0 +1,5 @@
+R1=13599840256; %R of origin planet
+R2=9832684544;%R of Destination(Example is EVE)
+Theta=pi-pi*sqrt(((R1+R2)^3)/(2*R2^3))/2;
+
+Theta=rad2deg(Theta) %anticlockwise direction of kerbin orbit is plus
